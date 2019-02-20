41ddcc0a9863e02959fa2519711ad5214e7813de
Deas McMorrow <yougotketchuponit@gmail.com>
2019-02-20 12:57:19 +0000

    quiz

--- plantmateApp/urls.py
+++ plantmateApp/urls.py
@@ -3,5 +3,8 @@ from plantmateApp import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^$', views.home, name='quiz'),
+    url(r'^$', views.quiz, name='quiz'),
+    url(r'^$', views.recommendations, name='recommendations'),
+    url(r'^$', views.login, name='login'),
+    url(r'^$', views.signup, name='signup'),
 ]
--- plantmateApp/views.py
+++ plantmateApp/views.py
@@ -6,3 +6,12 @@ def home(request):
 
 def quiz(request):
     return HttpResponse("plantmate quiz")
+
+def recommendations(request):
+    return HttpResponse("recommendations")
+
+def login(request):
+    return HttpResponse("login save")
+
+def signup(request):
+    return HttpResponse("signup save")
