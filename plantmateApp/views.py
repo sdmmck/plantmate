c8b3d0a83375aaaf7cc789175ee0dc41a314f20f
sdmmck <sarahdimmock93@gmail.com>
2019-02-20 12:25:40 +0000

    added plant page

--- plantmateApp/views.py
+++ plantmateApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def plant(request):
+    return HttpResponse("This is a plant!")
+
+
+def plant_list(request):
+
 
 # Create your views here.
