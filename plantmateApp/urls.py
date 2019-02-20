05fc659fcd0c17b410a60f53776d9e350a4dc12d
sdmmck <sarahdimmock93@gmail.com>
2019-02-20 12:58:35 +0000

    wishlist and myplants set up

--- plantmateApp/urls.py
+++ plantmateApp/urls.py
@@ -4,5 +4,7 @@ from plantmateApp import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^plant/', views.plant, name='plant'),
-    url(r'^plant-list/', views.plantList, name='plantList')
+    url(r'^plant-list/', views.plant_list, name='plant_list'),
+    url(r'^myaccount/wishlist/', views.wishlist, name='wishlist'),
+    url(r'^myaccount/my-plants/', views.my_plants, name='my_plants'),
 ]
